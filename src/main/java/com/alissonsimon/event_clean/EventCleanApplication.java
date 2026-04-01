44a1d3b51f9b04518ae8262e856749302fb36868
Alisson Simon <alissonsimon21@gmail.com>
2026-04-01 14:50:51 -0300

    feat: creating project

---
+++ src/main/java/com/alissonsimon/event_clean/EventCleanApplication.java
@@ -0,0 +1,11 @@
+package com.alissonsimon.event_clean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCleanApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EventCleanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alissonsimon/event_clean/EventCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alissonsimon.event_clean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
