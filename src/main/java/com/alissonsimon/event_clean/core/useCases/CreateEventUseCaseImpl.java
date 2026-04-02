37b984862e2401b0575f3d5b5409a931c70c0bda
Alisson Simon <alissonsimon21@gmail.com>
2026-04-02 14:37:32 -0300

    fix (core): adjusting CreateEventUseCaseImpl execute method return

--- src/main/java/com/alissonsimon/event_clean/core/useCases/CreateEventUseCaseImpl.java
+++ src/main/java/com/alissonsimon/event_clean/core/useCases/CreateEventUseCaseImpl.java
@@ -12,6 +12,6 @@ public class CreateEventUseCaseImpl implements CreateEventUseCase {
 
     @Override
     public Event execute(Event event) {
-        return null;
+        return eventGateway.createEvent(event);
     }
 }
