d5bd483157de7874539b286a1f2a3185df968484
Alisson Simon <alissonsimon21@gmail.com>
2026-04-02 14:34:37 -0300

    feat (infra): creating event repository

---
+++ src/main/java/com/alissonsimon/event_clean/infra/persistence/EventRepository.java
@@ -0,0 +1,6 @@
+package com.alissonsimon.event_clean.infra.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<EventEntity, Long> {
+}
